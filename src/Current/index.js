d855377715089070a8088a8d08aa09f5677a373e
lscheinhorn <lukescheinhorn@gmail.com>
2022-06-14 13:21:17 -0400

    feat: interval option' '

--- src/Current/index.js
+++ src/Current/index.js
@@ -3,14 +3,77 @@ import './style.css'
 export default function Current (props) {
     const { current } = props
 
-    const time = current.Time
+    const getDate = () => {
+        let arr = current.Time.split(/-|\s|:/)
+        let date = new Date(arr[0], arr[1] -1, arr[2], arr[3], arr[4])
+        
+        console.log(date)
+        return date
+    }
+    const date = getDate()
+    let meridian
+    const getHours = () => {
+        let hours = date.getHours()
+        if ( hours < 13 ) {
+            meridian = hours !== 12 ? "a.m." : "p.m."
+            if ( hours < 1 ) {
+                hours = 12
+            }
+            return hours
+        } else {
+            hours -= 12
+            meridian = "p.m."
+            return hours
+        }
+    }
+    const timeString = getHours() + ":" + (date.getMinutes() < 10 ? date.getMinutes() + "0" : date.getMinutes()) + " " + meridian
     const type = current.Type
     const velocity = current.Velocity_Major
 
-    console.log("current", current)
+    const getPrompt = () => {
+        const getDay = () => {
+            const now = new Date()
+            console.log("now.getDate()", now.getDate())
+            if ( now.getDate() === date.getDate() ) {
+                return "Today"
+            } else if (now.getDate() === date.getDate() - 1 ) {
+                return "Tomorrow"
+            } else {
+                return date.getDate()
+            }
+        }
+        const day = getDay()
+
+        const getCurrentType = () => {
+            if ( velocity < -0.1 ) {
+                return "ebbing at "
+            } else if ( velocity > 0.1 ) {
+                return "flooding at "
+            } else {
+                return "slack tide"
+            }
+        }
+
+        const currentType = getCurrentType()
+
+        const getCurrentSpeed = () => {
+            if ( velocity < 0.1 && velocity > -0.1) {
+                return ""
+            } else {
+                return Math.abs(velocity) + " knots"
+            }
+        }
+
+        const currentSpeed = getCurrentSpeed()
+
+        return `At ${timeString} it is ${currentType}${currentSpeed}`
+    }
+    
+    const prompt = getPrompt()
+
     return (
         <div className="current" >
-            <p>{time}: {type} at {velocity} knots</p>
+            <p>{ prompt }</p>
         </div>
     )
 }
--- src/Data/index.js
+++ src/Data/index.js
@@ -4,6 +4,19 @@ import { useEffect, useState } from 'react'
 
 export default function Data () {
     const [ currents, setCurrents ] = useState()
+    const [queryParams, setQueryParams] = useState({
+        begin_date: null,
+        end_date: null,
+        range: null,
+        date: "today",
+        station: "HUR0611",
+        product: "currents_predictions",
+        units: "english",
+        time_zone: "lst_ldt",
+        application: "luke_scheinhorn",
+        format: "json",
+        interval: "MAX_SLACK"
+    })
 
     useEffect(() => {
         const getDateTime = () => {
@@ -11,33 +24,24 @@ export default function Data () {
             const year = now.getFullYear()
             const month = (now.getMonth() + 1) < 10 ? "0" + (now.getMonth() + 1) : (now.getMonth() + 1)
             const day = now.getDate()
-            const hour = now.getHours()
+            const hour = now.getHours() < 10 ? "0" + now.getHours() : now.getHours()
             const min = now.getMinutes()
-            const date = month + '/' + day + '/' + year + '+' + hour + ':' + min
-            return date
+            const currentDate = month + '/' + day + '/' + year + '+' + hour + ':' + min
+            return currentDate
         }
         
         const now = getDateTime()
 
-        const queryParams = {
-            begin_date: now,
-            range: "24",
-            station: "HUR0611",
-            product: "currents_predictions",
-            units: "english",
-            time_zone: "lst_ldt",
-            application: "luke_scheinhorn",
-            format: "json",
-            interval: "MAX_SLACK"
-        }
-    
         const getUrl = () => {
             let urlArr = ["https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?"]
             for ( const param in queryParams ) {
-                urlArr.push(param)
-                urlArr.push("=")
-                urlArr.push(queryParams[param])
-                urlArr.push("&")
+                if ( queryParams[param] ) {
+                    urlArr.push(param)
+                    urlArr.push("=")
+                    urlArr.push(queryParams[param])
+                    urlArr.push("&")
+                }
+                
             }
             urlArr.pop()
             const url = urlArr.join("")
@@ -54,16 +58,41 @@ export default function Data () {
         }
 
         getCurrents()
-    }, []
+    }, [queryParams]
     )
 
     useEffect(() => {
         console.log(currents)
     },  [currents])
 
+    const handleIntervalChange = ({ target }) => {
+        const value = target.value
+        setQueryParams( prevState => {
+            return {
+                ...prevState,
+                interval: value
+            }
+        })
+    }
+
+    const handleDateChange = ({ target }) => {
+        const value = target.value
+        setQueryParams( prevState => {
+            return {
+                ...prevState,
+                date: value
+            }
+        })
+    }
+
     return (
         <div className="data" >
-        <h1>Predicted currents at the George Washington Bridge for the following 24 hours</h1>
+        <h1>Today's predicted currents at the George Washington Bridge</h1>
+            <select value={ queryParams.interval } onChange={ handleIntervalChange } title="Time interval">
+                <option value="MAX_SLACK">Slack & Max Flodd / Ebb</option>
+                <option value="30">30 Minutes</option>
+                <option value="60">1 Hour</option>
+            </select>
             {
                 currents?.map(current => <Current current={ current } key={current.Time}/>)
             }
