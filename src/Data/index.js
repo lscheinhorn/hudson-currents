a3249e86d219eaa0aa9de0aee5a47d7a86ea854c
lscheinhorn <lukescheinhorn@gmail.com>
2022-06-16 10:32:00 -0400

    feat: last refresh

--- src/Data/index.js
+++ src/Data/index.js
@@ -1,9 +1,13 @@
 import Current from '../Current'
 import './style.css'
 import { useEffect, useState } from 'react'
+import Clock from 'react-live-clock'
 
 export default function Data () {
     const [ currents, setCurrents ] = useState()
+    const [ dateRefreshed, setDateRefreshed ] = useState(new Date())
+    const [ userLocation, setUserLocation ] = useState()
+
     const [queryParams, setQueryParams] = useState({
         begin_date: null,
         end_date: null,
@@ -18,7 +22,92 @@ export default function Data () {
         interval: "MAX_SLACK"
     })
 
+    const getDay = () => {
+        let day
+        switch ( dateRefreshed.getDay() ) {
+            case 0:
+                day = "Sunday"
+                break
+            case 1:
+                day = "Monday"
+                break
+            case 2:
+                day = "Tuesday"
+                break
+            case 3:
+                day = "Wednesday"
+                break
+            case 4:
+                day = "Thursday"
+                break
+            case 5:
+                day = "Friday"
+                break
+            case 6:
+                day = "Saturday"
+                break
+            default:
+                day = ""
+        }
+        return day
+    }
+
+    const getMonth = () => {
+        let month
+        switch ( dateRefreshed.getMonth() ) {
+            case 0:
+                month = "January"
+                break
+            case 1:
+                month = "Febuary"
+                break
+            case 2:
+                month = "March"
+                break
+            case 3:
+                month = "April"
+                break
+            case 4:
+                month = "May"
+                break
+            case 5:
+                month = "June"
+                break
+            case 6:
+                month = "July"
+                break
+            case 7:
+                month = "August"
+                break
+            case 8:
+                month = "September"
+                break
+            case 9:
+                month = "October"
+                break
+            case 10:
+                month = "November"
+                break
+            case 11:
+                month = "December"
+                break
+            default:
+                month = ""
+        }
+        return month
+    }
+
+    const getYear = () => {
+        const year = dateRefreshed.getFullYear().toString()
+        return year
+    }
+
+    let todaysDate = `${getDay()} ${getMonth()} ${dateRefreshed.getDate()}`
+    console.log(todaysDate)
+
     useEffect(() => {
+        // getLocation()
+
         const getDateTime = () => {
             const now = new Date()
             const year = now.getFullYear()
@@ -30,7 +119,8 @@ export default function Data () {
             return currentDate
         }
         
-        const now = getDateTime()
+        const nowString = getDateTime()
+
 
         const getUrl = () => {
             let urlArr = ["https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?"]
@@ -49,12 +139,12 @@ export default function Data () {
         }
 
         const getCurrents = async () => {
-            let data = await fetch(getUrl())
+            let data = await fetch(getUrl()/*, {mode: "no-cors"}*/)
                 .then(response => response.json())
                 .then(json => json);
             data = data["current_predictions"]["cp"]
             setCurrents(data)
-            console.log(currents)
+            console.log("currents", currents)
         }
 
         getCurrents()
@@ -84,18 +174,60 @@ export default function Data () {
             }
         })
     }
+    
+
+    const reload = () => window.location.reload(false)
+
+
+    // const getLocation = () => {
+    //     navigator.geolocation.getCurrentPosition( 
+    //         function(location) { 
+    //             setUserLocation(location.coords)
+    //         });
+    // }
+    
+    // useEffect(() => {
+    //     console.log("userLocation", userLocation.latitude, userLocation.longitude)
+    // }, [userLocation] )
 
     return (
         <div className="data" >
-        <h1>Today's predicted currents at the George Washington Bridge</h1>
+        <h1>Predicted Currents</h1>
+        <h2>Station: George Washington Bridge</h2>
+        <div className="side-by-side">
+            <div className="container">
+                <h3>Last refresh: </h3>
+                <Clock format={'ddd MMM D'} timezone={'US/Eastern'}></Clock>
+                <div>
+                    <Clock format={'h:mm a'} timezone={'US/Eastern'}></Clock>
+                </div>                
+                <div>
+                    <button className="btn btn-primary" onClick={ reload }>Refresh</button>
+                </div>
+            </div>
+            <div className="container">
+                <h3>Current date:</h3>
+                <div>
+                    <Clock format={'ddd MMM D'} ticking={true} timezone={'US/Eastern'}></Clock>
+                </div>
+                <div>
+                    <Clock className="large-bold" format={'h:mm a'} ticking={true} timezone={'US/Eastern'}></Clock>
+                </div>
+            </div>
+        </div>
+        <div className="space-around">
             <select value={ queryParams.interval } onChange={ handleIntervalChange } title="Time interval">
-                <option value="MAX_SLACK">Slack & Max Flodd / Ebb</option>
-                <option value="30">30 Minutes</option>
-                <option value="60">1 Hour</option>
-            </select>
+                    <option value="MAX_SLACK">Slack & Max Flood / Ebb</option>
+                    <option value="30">30 Minutes</option>
+                    <option value="60">1 Hour</option>
+                </select>
+        </div>
+            
+            
             {
                 currents?.map(current => <Current current={ current } key={current.Time}/>)
             }
+            
         </div>
     )
 }
